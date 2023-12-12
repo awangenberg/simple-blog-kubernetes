fb0cf52b42e49bb7516aa900fdc3dd1d5892230f
Adam Wångenberg <adam.wangenberg@invidem.com>
2023-12-12 19:43:12 +0100

    add health check

--- app.py
+++ app.py
@@ -8,6 +8,13 @@ def helloworld():
     if(request.method == 'GET'): 
         data = {"data": "Hello World"} 
         return jsonify(data) 
-    
+
+@app.route('/health-check', methods=['GET']) 
+def health_check(): 
+    if(request.method == 'GET'): 
+        data = {"health-check": "Service is up and running!"} 
+        return jsonify(data)
+
 if __name__ == '__main__': 
     app.run(debug=True) 
+
