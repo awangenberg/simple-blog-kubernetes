33a939cbe6c8cdc1b3ced6774c7b0fbcd3589871
Adam Wångenberg <adam.wangenberg@invidem.com>
2023-12-15 13:42:32 +0100

    temp: call api on localhost

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const text = getHealthCheck()
+  const [text, setText] = useState(null);
+  
+  getHealthCheck().then(result => {
+    console.log(result);
+    setText(result);
+  }).catch(error => {
+    console.error(error);
+  });
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           {text}
+           {JSON.stringify(text)}
         </p>
         <a
           className="App-link"
@@ -25,13 +33,18 @@ function App() {
   );
 }
 
-const getHealthCheck = ()  => {
-  fetch("http://127.0.0.1:5000/health-check")
-    .then((response) => {
-        return response.json();
-    })
+const getHealthCheck = async () => {
+    try {
+      const response =  await fetch('http://127.0.0.1:5000/health-check');
 
-    return "ASD"; 
+      if(response.ok){
+        return await response.json();
+      }
+
+    } catch (error) {
+      console.error(error);
+    }
+  
 };
 
 export default App;
