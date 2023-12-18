827d92853b3c1ad6885ed081c8c9530ba3ed6263
Adam Wångenberg <adam.wangenberg@invidem.com>
2023-12-18 16:30:23 +0100

    some fixes

--- api/app.py
+++ api/app.py
@@ -1,26 +1,19 @@
 from flask import Flask, jsonify, request
+import flask
 from flask_cors import CORS 
 
 
 app =   Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "HEAD", "POST", "OPTIONS"], "allow_headers": ["*"]}})
 
-@app.route('/posts', methods=['POST']) 
-def create_posts(): 
-    if(request.method == 'POST'): 
-        heading = request.form['heading']
-        body = request.form['body']
-        
-        return jsonify(data) 
 
 @app.route('/health-check', methods=['GET']) 
 def health_check(): 
     if(request.method == 'GET'): 
-        data = {"health-check": "The service is up and running!"} 
-        return jsonify(data)
-
+        response = flask.jsonify({'health-check': 'The service is up and running!'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
 
 if __name__ == '__main__': 
     app.run(debug=True) 
-
--- frontend/startingpoint.sh
+++
@@ -1,7 +0,0 @@
-# __conf="
-# window.appEnv = {
-#     REACT_APP_BACKEND_URL: '$REACT_APP_BACKEND_URL'
-# }
-# "
-#echo "$__conf" > build/scripts/config.js
-# npx serve build
