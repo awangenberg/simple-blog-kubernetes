8a26dff4e5ab1f181cf9fdbf30f9ac74b090a623
Adam Wångenberg <adam.wangenberg@invidem.com>
2023-12-19 23:27:20 +0100

    fix prod config for mysql

--- api/config.py
+++ api/config.py
@@ -4,9 +4,10 @@ class Config:
     DEBUG = False
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') #TODO: get from kubernetes
+    sql_password = username = os.environ["MYSQL_ROOT_PASSWORD"]
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mysql:' + sql_password + '@10.128.81.29:3306/simple_blog'
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI= 'mysql+pymysql://root:password@localhost:3306/simple-blog'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost:3306/simple_blog'
     DEBUG = True
     DEVELOPMENT = True
