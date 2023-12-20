d1079bde033b067b6ffa936cd8aac7d1cf65d7ea
Adam Wångenberg <adam.wangenberg@invidem.com>
2023-12-20 14:23:31 +0100

    use server name instead of IP when connecting to mySql

--- api/config.py
+++ api/config.py
@@ -5,16 +5,10 @@ class Config(object):
 
 class ProductionConfig(Config):
     sql_password = os.environ["MYSQL_ROOT_PASSWORD"]
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://mysql:" + sql_password + "@10.128.81.29:3306/simple_blog"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:" + sql_password + "@mysql:3306/simple_blog"
 
 class DevelopmentConfig(Config):
     sql_password = os.environ["MYSQL_ROOT_PASSWORD"]
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:" + sql_password + "@localhost:3306/simple_blog"
     DEBUG = True
     DEVELOPMENT = True
-    
-    
-app_config = {
-        'development': DevelopmentConfig,
-        'production': ProductionConfig
-   }
