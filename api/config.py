77138d9b1d0450f32e376f58cccdf419bc2de2bd
Adam Wångenberg <adam.wangenberg@invidem.com>
2023-12-20 13:18:09 +0100

    install cryptography

--- api/config.py
+++ api/config.py
@@ -5,11 +5,11 @@ class Config(object):
 
 class ProductionConfig(Config):
     sql_password = os.environ["MYSQL_ROOT_PASSWORD"]
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mysql:' + sql_password + '@10.128.81.29:3306/simple_blog'
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://mysql:" + sql_password + "@10.128.81.29:3306/simple_blog"
 
 class DevelopmentConfig(Config):
     sql_password = os.environ["MYSQL_ROOT_PASSWORD"]
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:' + sql_password + '@localhost:3306/simple_blog'
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:" + sql_password + "@localhost:3306/simple_blog"
     DEBUG = True
     DEVELOPMENT = True
     
